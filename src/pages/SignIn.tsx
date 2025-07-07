e01f92223558f24f8fde371806fb462246e51cbe
PauloHBSF <paulohbsfilho@gmail.com>
2025-07-06 23:47:58 -0300

    :sparkles: implementado a estrutua da tela de login

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,12 @@
+import { Input } from "../components/Input"
+import { Button } from "../components/Button"
+
 export function SignIn() {
-    return <h1>Sign In</h1>;
+    return (
+        <form className="w-full flex flex-col gap-4">
+            <Input legend="E-mail" required type="email" placeholder="seu@email.com" />
+            <Input legend="Senha" required type="password" placeholder="senha" />
+            <Button>Entrar</Button>
+        </form>
+    )
 }
