47bdb847587cb5afbce162aed66fcf6549685323
PauloHBSF <paulohbsfilho@gmail.com>
2025-07-07 00:21:11 -0300

    :sparkles: Criando página de SignUp

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router";
-import { SignIn } from "../pages/SignIn";
 import { AuthLayout } from "../components/AuthLayout";
+
+import { SignIn } from "../pages/SignIn";
+import { SignUp } from "../pages/SignUp"
 
 export function AuthRoutes() {
     return (
         <Routes>
             <Route path="/" element={<AuthLayout />}>
                 <Route path="/" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
             </Route>
         </Routes>
     );
