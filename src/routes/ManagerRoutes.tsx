342e3f5849b0afebc6a5dbb7419d1a7595c2ed23
PauloHBSF <paulohbsfilho@gmail.com>
2025-07-09 16:38:45 -0300

    :sparkles: comecando o dashboard do manager

---
+++ src/routes/ManagerRoutes.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router";
+import { AppLayout } from "../components/AppLayout";
+import { Dashboard } from "../pages/Dashboard";
+import { NotFound } from "../pages/NotFound";
+
+export function ManagerRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<AppLayout />}>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
+    );
+}
